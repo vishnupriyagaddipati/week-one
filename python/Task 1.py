7ab73373291cce6a5a6f010d78b470581a037722
vishnupriyagaddipati <vishnupriyagaddipati@gmail.com>
2025-11-02 21:48:39 +0530

    Add files via upload

---
+++ python/Task 1.py
@@ -0,0 +1,7 @@
+number1=int(input("Enter the first number:"))
+number2=int(input("Enter the second number:"))
+
+print(f"Addition :{number1+number2}")
+print(f"Subtraction: {number1-number2}")
+print(f"Multiplication: {number1*number2}")
+print(f"Division :{number1/number2}")
---
+++ python/Task 2.py
@@ -0,0 +1,3 @@
+FirstName =input("Enter the first name:")
+LastName=input("Enter the last name:")
+print(f"Hello, {FirstName} {LastName}! Welcome to python programming")
